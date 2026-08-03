e6606c3ba1314d947a1193d688e9a45d0febd55e
tiemokocamara <mtiecamou@gmail.com>
2026-08-04 00:11:48 +0200

    feat add accessible display zoom controls

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,12 +3,13 @@
 // import 'bootstrap'
 // import '../css/app.css'; // importe tout le CSS compilé
 
-import { createApp, h } from 'vue'
+import { createApp, Fragment, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { createPinia } from 'pinia'
 import i18n from './i18n'
 import { InertiaProgress } from '@inertiajs/progress'
+import AccessibilityZoom from './Components/AccessibilityZoom.vue'
 
 // Active la barre de progression lors de la navigation
 InertiaProgress.init()
@@ -21,7 +22,12 @@ createInertiaApp({
         ),
 
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+        createApp({
+            render: () => h(Fragment, null, [
+                h(App, props),
+                h(AccessibilityZoom),
+            ]),
+        })
             .use(plugin)
             .use(createPinia())
             .use(i18n)
